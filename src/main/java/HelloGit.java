06c71635ac2076e8fcbd7e70b0de9e285d47ba67
ramazancoool <ramazancoool@gmail.com>
2022-11-10 15:47:17 +0300

    second commit

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -1,5 +1,10 @@
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        hiMethod();
+    }
+
+    public static void hiMethod(){
+        System.out.println("hi method");
     }
 }
---
+++ src/main/java/OtherClass.java
@@ -0,0 +1,8 @@
+public class OtherClass {
+    private String name;
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
